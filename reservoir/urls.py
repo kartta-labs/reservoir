64177942a36a843abadb0b6fc06e81f812c51c6e
Brandon Mayer <bmayer@google.com>
2020-08-18 11:48:12 -0400

    Adding apache 2.0 header.

--- reservoir/urls.py
+++ reservoir/urls.py
@@ -1,3 +1,17 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """reservoir URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -15,6 +29,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from third_party.3dmr import urls as 3dmr_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
