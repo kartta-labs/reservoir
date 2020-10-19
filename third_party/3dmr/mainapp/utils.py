092785dbefcde554cde97536e884268d651d9e46
Brandon Mayer <b.mayer1@gmail.com>
2020-10-19 18:18:53 +0000

    Safe to push single ODL liciense to prod to fix upload page.

--- third_party/3dmr/mainapp/utils.py
+++ third_party/3dmr/mainapp/utils.py
@@ -24,18 +24,12 @@ def admin(request):
 
 
 LICENSES_FORM = {
-#    0: mark_safe('<a href="https://creativecommons.org/publicdomain/zero/1.0/deed.en">Creative Commons CC0 1.0 Universal Public Domain Dedication</a>'),
-#    1: mark_safe('<a href="https://creativecommons.org/licenses/by/4.0/deed.en">Creative Commons Attribution 4.0 International</a>, with attribution given to "3D Model Repository", and no further attribution')
     0: mark_safe('<a href="http://opendatacommons.org/licenses/odbl/1.0/">Open Database License</a>'),
-    1: mark_safe('<a href="http://opendatacommons.org/licenses/odbl/1.0/">Open Database License</a>')
 }
 
 # The same as above, for the model pages
 LICENSES_DISPLAY = {
-#    0: mark_safe('This 3d model is made available under the <a href="https://creativecommons.org/publicdomain/zero/1.0/deed.en">Creative Commons CC0 1.0 Universal Public Domain Dedication</a>.'),
-#    1: mark_safe('This 3d model is licensed under the <a href="https://creativecommons.org/licenses/by/4.0/deed.en">Creative Commons Attribution 4.0 International license</a>.')
     0: mark_safe('This 3d model is made available under the <a href="http://opendatacommons.org/licenses/odbl/1.0/">Open Database License</a>.'),
-    1: mark_safe('This 3d model is made available under the <a href="http://opendatacommons.org/licenses/odbl/1.0/">Open Database License</a>.')
 }
 
 # The possible changes users can make to the repository
