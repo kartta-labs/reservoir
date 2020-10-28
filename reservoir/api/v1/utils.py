54d04a271f795eea53778fe522e0039e9e73b7e0
Brandon Mayer <b.mayer1@gmail.com>
2020-10-28 19:27:46 +0000

    Automatically revise an existing model given an upload request keyed by building id.

---
+++ reservoir/api/v1/utils.py
@@ -0,0 +1,50 @@
+import importlib
+import logging
+
+logger = logging.getLogger(__name__)
+
+mainapp_models = importlib.import_module('third_party.3dmr.mainapp.models')
+mainapp_model = getattr(mainapp_models, 'Model')
+
+
+def build_revision_options(model, model_file, validated_data, author):
+    """Build an options dictionary given client supplied validated metdata,
+    |validated_data|. |model| is an instance of 3dmr.mainapp.models.Model.
+    """
+
+    options = {
+        'revision': True,
+        'model_id': model.model_id,
+        'model_file': model_file
+    }
+
+    if validated_data.get('title') and validated_data.get('title') != model.title:
+        options['title'] = validated_data.get('title')
+
+    if validated_data.get('latitude') and validated_data.get('longitude'):
+        if validated_data.get('latitude') != model.location.latitude or \
+           validated_data.get('longitude') != model.location.longitude:
+            options['latitude'] = validated_data.get('latitude')
+            options['longitude'] = validated_data.get('longitude')
+
+    if validated_data.get('tags'):
+        options['tags'] = validated_data.get('tags')
+
+    if validated_data.get('origin'):
+        options['origin'] = validated_data.get('origin')
+
+    if validated_data.get('translation'):
+        options['translation'] = validated_data.get('translation')
+
+    if validated_data.get('rotation'):
+        options['rotation'] = validated_data.get('rotation')
+
+    if validated_data.get('scale'):
+        options['scale'] = validated_data.get('scale')
+
+    if validated_data.get('license'):
+        options['license'] = validated_data.get('license')
+
+    options['author'] = author
+
+    return options
